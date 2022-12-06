7ac588f7c039c41a9bc772862c0465ec456b2eb2
xeno <xeno@lv427.space>
2022-12-06 06:15:13 +0100

    Day 6 part 1 and 2

---
+++ Program.fs
@@ -0,0 +1,16 @@
+﻿open System.IO
+
+let lines = File.ReadAllLines "/tmp/aoc/input" |> Array.toList |> List.map (fun s -> s.ToCharArray() |> Array.toList)
+
+lines |> List.map (printfn "%A")  // findIndex(Back)=indexOfFirst, collect=flatMap, fold*
+
+let rec skipToMarker (str: char list) =
+    let s = str |> List.take 14 |> Set.ofList
+    if s |> Set.count = 14 then 14
+    else 1 + (str.Tail |> skipToMarker)
+    
+let skipped = lines |> List.map skipToMarker
+
+skipped |> List.map (printfn "%A")
+
+
